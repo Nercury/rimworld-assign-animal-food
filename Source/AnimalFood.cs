6ee685d794d69d19ed1708260682e1183465f96b
Nerijus Arlauskas <nercury@gmail.com>
2021-06-21 22:11:44 +0300

    Initial files

---
+++ Source/AnimalFood.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace AssignAnimalFood
+{
+    [StaticConstructorOnStartup]
+    public static class AssignAnimalFoodMod
+    {
+        static AssignAnimalFoodMod()
+        {
+            new Harmony(AnimalMod.Id).PatchAll();
+            Logger.Message("Initialized");
+        }
+    }
+}
---
+++ Source/Mod.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace AssignAnimalFood
+{
+    public class AnimalMod
+    {
+        public const string Id = "Everyone.AssignAnimalFood";
+        public const string Name = "Assign Animal Food";
+        public const string Version = "1.0";
+
+        public static string FoodRestrictionAnimal 
+            => "FoodRestrictionAnimal".TryTranslate(out TaggedString t) ? t.RawText : "Animal";
+    }
+}
