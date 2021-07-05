9b46bfa8d6dcc0790dcb4b5d27ea18f69042a6cf
Nerijus Arlauskas <nercury@gmail.com>
2021-07-05 23:06:47 +0300

    1.3 version

---
+++ Source/1.3/Mod.cs
@@ -0,0 +1,11 @@
+﻿using Verse;
+
+namespace AssignAnimalFood
+{
+    public class AnimalMod
+    {
+        public const string Id = "Everyone.AssignAnimalFood";
+        public const string Name = "Assign Animal Food";
+        public const string Version = "1.2.0";
+    }
+}
